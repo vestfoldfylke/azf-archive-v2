378ff207bf60d54c3cab74800dabbce812f8e715
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-08-26 21:19:44 +0200

    fix: refactor access token decoding to use custom decodeJwt function and remove jsonwebtoken dependency

--- lib/decode-access-token.ts
+++ lib/decode-access-token.ts
@@ -1,5 +1,17 @@
-import { decode, type JwtPayload } from "jsonwebtoken";
 import type { DecodedAccessToken } from "../types/auth.js";
+
+type JwtPayload = {
+  upn: string | undefined
+  appid: string | undefined
+  roles: string[] | undefined
+}
+
+// We only decode, as built in entra auth verifies. Decode only for metadata - not authentication.
+const decodeJwt = (token: string): JwtPayload => {
+  const base64Payload = token.replace("Bearer ", "").split(".")[1]
+  const payload = Buffer.from(base64Payload, "base64url").toString()
+  return JSON.parse(payload) as JwtPayload
+}
 
 export const decodeAccessToken = (token: string | null): DecodedAccessToken => {
   const result: DecodedAccessToken = {
@@ -17,7 +29,7 @@ export const decodeAccessToken = (token: string | null): DecodedAccessToken => {
 
   let decoded: JwtPayload | string | null;
   try {
-    decoded = decode(token.replace("Bearer ", ""));
+    decoded = decodeJwt(token);
   } catch (_error) {
     result.msg = "Token is not a valid jwt";
     return result;
