721abd560d0f4f8f84f8f5e89d5f38d922a7de7d
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-08-27 09:11:34 +0200

    fix: remove node-cache dependency and use built in mem-cache in msal-node

--- lib/get-entra-id-token.ts
+++ lib/get-entra-id-token.ts
@@ -1,41 +1,38 @@
 import { type AuthenticationResult, type ClientCredentialRequest, ConfidentialClientApplication, type Configuration } from "@azure/msal-node";
 import { logger } from "@vestfoldfylke/loglady";
-import NodeCache from "node-cache";
 import { APPREG_CLIENT } from "../config.js";
 
-const cache = new NodeCache({ stdTTL: 4000 });
-
-async function getToken(scope: string, options: { forceNew: boolean } = { forceNew: false }): Promise<string> {
-  const cacheKey: string = scope;
-
-  if (!options.forceNew && cache.get(cacheKey)) {
-    logger.info("getEntraIdToken - found valid access value in cache, will use that instead of fetching new");
-    return cache.get(cacheKey) as string;
+const config: Configuration = {
+  auth: {
+    clientId: APPREG_CLIENT.clientId,
+    authority: `https://login.microsoftonline.com/${APPREG_CLIENT.tenantId}`,
+    ["clientSecret"]: APPREG_CLIENT.clientSecret
   }
+};
 
-  logger.info("getEntraIdToken - no access value in cache, fetching new from Microsoft");
-  const config: Configuration = {
-    auth: {
-      clientId: APPREG_CLIENT.clientId,
-      authority: `https://login.microsoftonline.com/${APPREG_CLIENT.tenantId}`,
-      ["clientSecret"]: APPREG_CLIENT.clientSecret
-    }
-  };
+const confidentialClientApplication = new ConfidentialClientApplication(config);
 
-  const cca = new ConfidentialClientApplication(config);
+async function getToken(scope: string): Promise<string> {
   const clientCredentials: ClientCredentialRequest = {
     scopes: [scope]
   };
 
-  const result: AuthenticationResult | null = await cca.acquireTokenByClientCredential(clientCredentials);
-  if (!result?.accessToken || !result.expiresOn) {
+  const result: AuthenticationResult | null = await confidentialClientApplication.acquireTokenByClientCredential(clientCredentials);
+  if (!result?.accessToken) {
     throw new Error("getEntraIdToken - Failed to acquire access value from Microsoft");
+  }
+
+  if (result.fromCache) {
+    logger.info("getEntraIdToken - Got token from ConfidentialClientApplication cache");
+    return result.accessToken;
+  }
+
+  if (!result.expiresOn) {
+    throw new Error("getEntraIdToken - Got new token, but no expiration, that cant be right...");
   }
 
   const expires: number = Math.floor((result.expiresOn.getTime() - Date.now()) / 1000);
   logger.info("getEntraIdToken - Got Microsoft access value, expires in {Expires} seconds.", expires);
-  cache.set(cacheKey, result.accessToken, expires);
-  logger.info("getEntraIdToken - Access value stored in cache");
 
   return result.accessToken;
 }
